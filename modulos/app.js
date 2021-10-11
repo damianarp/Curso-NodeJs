cfc9ffef1d0061bc3cd8fc3143604662bbce5968
damianarp <damianarp@gmail.com>
2021-10-11 14:02:57 -0300

    carga de módulos

---
+++ modulos/app.js
@@ -0,0 +1,6 @@
+const dato = require('./datos');
+
+dato('Hola mundo');
+
+// Para ver el log del módulo en consola.
+console.log(module);
---
+++ modulos/datos.js
@@ -0,0 +1,8 @@
+var url = 'http://damianarp.com';
+
+function dato(mensaje){
+    //Cuando se envíe un HTTP Request
+    console.log(mensaje);
+}
+
+module.exports = dato;

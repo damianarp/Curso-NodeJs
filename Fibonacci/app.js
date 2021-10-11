eefce75b808f62b70e4085436652eed0b6b7b39b
damianarp <damianarp@gmail.com>
2021-10-11 17:54:55 -0300

    Pasando la cantidad de elementos de la serie desde el terminal

--- Fibonacci/app.js
+++ Fibonacci/app.js
@@ -4,8 +4,23 @@
 // Importamos serie.js
 const serie = require('./serie');
 
+// Declaramos una variable para capturar los argv con el método process.
+let argv = process.argv; // Salida: ['ruta donde esta instalado node','ruta del archivo app.js']
+
+// Desde el terminal se pueden agregar mas párametros, por ejemplo:
+// node app.js serie=5
+// Salida: ['ruta donde esta instalado node','ruta del archivo app.js', serie=5]
+
+// Capturamos el índice 2 para obtener el tercer elemento del arreglo.
+// Separamos el valor con un split para obtenerlo -> Salida: ['serie', '5']
+// y obtenemos el valor pasándole el índice -> Salida: 5
+let valor = argv[2].split('=')[1];
+
+// Podemos comprobar que funciona con un console.log
+//console.log(valor);
+
 // Definimos la cantidad de números de la serie
-let cantidad = 20;
+let cantidad = valor;
 
 // Llamamos al método crearSerie (que retorna una promesa) y lo manejamos.
 serie.crearSerie(cantidad)
